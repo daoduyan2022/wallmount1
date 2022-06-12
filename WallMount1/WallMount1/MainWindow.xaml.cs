e8fae1f7c10b7f3ec1c29ca69e67fe7f9e2c5e5b
An <mail@mail.com>
2022-06-13 01:38:19 +0700

    13062022

---
+++ WallMount1/WallMount1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WallMount1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_play_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Setup_Model setupModelWindow = new Setup_Model();
+            setupModelWindow.Show();
+        }
+    }
+}
---
+++ WallMount1/WallMount1/Setup_Model.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WallMount1
+{
+    /// <summary>
+    /// Interaction logic for Setup_Model.xaml
+    /// </summary>
+    public partial class Setup_Model : Window
+    {
+        public Setup_Model()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_sub_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectStream_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_subStream_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_addStream_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectStep_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_addStep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_subStep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_cam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_getImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectIndexRoi_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CamDevice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Port_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
